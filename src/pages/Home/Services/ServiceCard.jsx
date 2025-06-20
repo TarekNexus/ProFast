7db83a5fb9e30f209b727565b7ffe65a7549d6f6
Tarek592 <muhammadtarek5920@gmail.com>
2025-06-20 09:26:19 +0600

    Profast project setup complete

---
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ServiceCard = ({ service }) => {
+  const { icon: Icon, title, description } = service;
+  return (
+    <div className="bg-white hover:bg-[#CAEB66]  shadow-md rounded-2xl p-6 border  hover:shadow-lg transition-all duration-300">
+      <div className="text-4xl text-primary  mb-4">
+        <Icon />
+      </div>
+
+      <h2 className="text-xl font-semibold text-primary mb-2">{title}</h2>
+      <p className=" text-gray-600">{description}</p>
+    </div>
+  );
+};
+
+export default ServiceCard;
